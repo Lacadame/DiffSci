dcc100060eb19ed799ed40017609b29b0219e286
bepschaeffer <bepschaeffer@gmail.com>
2024-10-20 07:53:13 -0300

    Added loss weighting based on our Dkl bound

--- diffsci/models/karras/noisesamplers.py
+++ diffsci/models/karras/noisesamplers.py
@@ -3,6 +3,7 @@ from typing import Any
 import torch
 from torch import Tensor
 from jaxtyping import Float
+from scipy.integrate import quad
 
 
 class NoiseSampler(torch.nn.Module):
@@ -85,3 +86,60 @@ class VENoiseSampler(NoiseSampler):
         logsigma = logsigma_min + unif*(logsigma_max - logsigma_min)
         sigma = torch.exp(logsigma)
         return sigma
+
+
+class BoundNoiseSampler(NoiseSampler):
+    def __init__(self,
+                 sigma_data: float = 0.5,
+                 prior_mean: float = -1.2,
+                 prior_std: float = 1.2,
+                 R: float = 7,        # maybe use module.norm as a proxy for this when latent=True
+                 T: float = 80.0,      # get this from max_level noise in module
+                 delta: float = 0.5,
+                 normalization: float = 2.0):      # This is a hack
+        super().__init__()
+        self.register_buffer("sigma_data", torch.tensor(sigma_data))
+        self.register_buffer("prior_mean", torch.tensor(prior_mean))
+        self.register_buffer("prior_std", torch.tensor(prior_std))
+        self.register_buffer("R", torch.tensor(R))
+        self.register_buffer("T", torch.tensor(T))
+        self.register_buffer("delta", torch.tensor(delta))
+        self.register_buffer("normalization", torch.tensor(normalization))
+
+    def loss_weighting(self,
+                       sigma: Float[Tensor, '...'],
+                       ) -> Float[Tensor, '...']:
+        
+        # compute new weighting
+        rev_t = self.T - sigma    # Only the EDM noise_fn is implemented
+        R = self.R
+        C = 6*(4*R**2 + sigma**2) * torch.exp(4*R**2 / sigma**2)
+        inv_C = torch.where(C == float('inf'), torch.tensor(0.0), 1.0 / C)
+        integral = []
+        for i in range(rev_t.shape[0]):
+            integral.append(compute_integral(rev_t[i], self.T, inv_C[i], self.delta))
+        integral = torch.tensor(integral).to(rev_t)
+        new_weight = 1/(4*(sigma**2)*self.delta) * torch.exp(-integral)
+
+        # compute karras weighting
+        karras_weight = (sigma**2 + self.sigma_data**2)/((sigma*self.sigma_data)**2)
+
+        return karras_weight + self.normalization * new_weight
+
+    def sample(self,
+               shape: list[int]
+               ) -> Float[Tensor, '*shape']:  # noqa: F821
+        white_noise = torch.randn(shape).to(self.prior_mean.device)
+        logsigma = white_noise*self.prior_std + self.prior_mean
+        sigma = torch.exp(logsigma)
+        return sigma
+
+
+def integrand(rev_t, inv_C, T, delta=0.5):
+    t = T - rev_t
+    return 2 * inv_C * t * (1 - delta)
+
+
+def compute_integral(t_initial, t_final, inv_C, delta=0.5):
+    result, error = quad(integrand, t_initial, t_final, args=(inv_C, t_final, delta,))
+    return result
